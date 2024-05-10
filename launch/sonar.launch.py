1e01f5453cad5b184e9712577769a1a8a017d6eb
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-10 11:55:01 -0400

    Update sonar.launch.py

--- launch/sonar.launch.py
+++ launch/sonar.launch.py
@@ -2,6 +2,8 @@ import launch
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import LaunchConfigurationEquals
 from launch_ros.actions import Node
 
 def generate_launch_description():
