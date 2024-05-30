42a4859c9e274aa4a9aa1fa597498fd197658917
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-30 13:33:47 -0400

    change for timer

--- imagenex831l/sonar_node.py
+++ imagenex831l/sonar_node.py
@@ -26,26 +26,25 @@ class SonarNode(Node):
         self.range_raw_pub = self.create_publisher(RawRange, f'{device}/{SENSOR_NAME}/{SONAR_RAW_TOPIC_NAME}', 10)
         self.sonar_health = self.create_publisher(String, f'{device}/{SENSOR_NAME}/{SONAR_HEALTH_TOPIC_NAME}', 10)
         
-        
-        self.frequency = self.get_parameter('poll_frequency').value if self.has_parameter('poll_frequency') else POLL_FREQUENCY
         self.declare_parameters(
             namespace='',
             parameters=[
-            ('max_range', rclpy.Parameter.Type.INTEGER),
-            ('step_direction', rclpy.Parameter.Type.INTEGER),
-            ('start_gain', rclpy.Parameter.Type.INTEGER),
-            ('absorption', rclpy.Parameter.Type.INTEGER),
-            ('train_angle', rclpy.Parameter.Type.INTEGER),
-            ('sector_width', rclpy.Parameter.Type.INTEGER),
-            ('step_size', rclpy.Parameter.Type.INTEGER),
-            ('pulse', rclpy.Parameter.Type.INTEGER),
-            ('min_range', rclpy.Parameter.Type.INTEGER),
-            ('pitch_roll_mode', rclpy.Parameter.Type.INTEGER),
-            ('profile_mode', rclpy.Parameter.Type.INTEGER),
-            ('motor_mode', rclpy.Parameter.Type.INTEGER),
-            ('frequency', rclpy.Parameter.Type.INTEGER),
-            ('poll_frequency', rclpy.Parameter.Type.INTEGER)
+                ('max_range', rclpy.Parameter.Type.INTEGER),
+                ('step_direction', rclpy.Parameter.Type.INTEGER),
+                ('start_gain', rclpy.Parameter.Type.INTEGER),
+                ('absorption', rclpy.Parameter.Type.INTEGER),
+                ('train_angle', rclpy.Parameter.Type.INTEGER),
+                ('sector_width', rclpy.Parameter.Type.INTEGER),
+                ('step_size', rclpy.Parameter.Type.INTEGER),
+                ('pulse', rclpy.Parameter.Type.INTEGER),
+                ('min_range', rclpy.Parameter.Type.INTEGER),
+                ('pitch_roll_mode', rclpy.Parameter.Type.INTEGER),
+                ('profile_mode', rclpy.Parameter.Type.INTEGER),
+                ('motor_mode', rclpy.Parameter.Type.INTEGER),
+                ('frequency', rclpy.Parameter.Type.INTEGER),
+                ('poll_frequency', rclpy.Parameter.Type.INTEGER)
             ])
+        
         self.max_range = self.get_parameter('max_range').value
         self.step_direction = self.get_parameter('step_direction').value
         self.start_gain = self.get_parameter('start_gain').value
@@ -59,7 +58,7 @@ class SonarNode(Node):
         self.profile_mode = self.get_parameter('profile_mode').value
         self.motor_mode = self.get_parameter('motor_mode').value
         self.frequency = self.get_parameter('frequency').value
-        self.poll_frequency = self.get_parameter('poll_frequency').value
+        self.poll_frequency = self.get_parameter('poll_frequency').value if self.has_parameter('poll_frequency') else POLL_FREQUENCY
 
         self.sensor = Imagenex831L()
         self.get_logger().info(str(self.get_parameter('frequency').value))
@@ -67,60 +66,62 @@ class SonarNode(Node):
         self.first_exception_time = None
         self.sensor.set_parameters(self.step_direction, self.start_gain, self.absorption, self.train_angle, self.sector_width, self.step_size, self.pulse, self.max_range, self.min_range, self.pitch_roll_mode, self.profile_mode, self.motor_mode, self.frequency)
 
+        timer_period = 1.0 / self.poll_frequency  # Timer period in seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
     def parameters_callback(self, params):
         self.sensor.set_parameters(self.step_direction, self.start_gain, self.absorption, self.train_angle, self.sector_width, self.step_size, self.pulse, self.max_range, self.min_range, self.pitch_roll_mode, self.profile_mode, self.motor_mode, self.frequency)
         return SetParametersResult(successful=True)
 
+    def timer_callback(self):
+        sonar_msg = ProcessedRange()
+        sonar_raw_msg = RawRange()
+        current_time = self.get_clock().now()
 
-    def spin(self):
-        node = self.create_rate(self.poll_frequency)
-        while rclpy.ok():
-            sonar_msg = ProcessedRange()
-            sonar_raw_msg = RawRange()
-            current_time = self.get_clock().now()
+        try:
+            self.sensor.send_request()
+            raw_data = self.sensor.read_data()
 
-            try:
-                self.sensor.send_request()
-                raw_data = self.sensor.read_data()
+            sonar_raw_msg.header.stamp = current_time.to_msg()
+            sonar_raw_msg.header.frame_id = 'sonar'
+            sonar_raw_msg.data = raw_data
+            self.get_logger().debug(str(sonar_raw_msg))
 
-                sonar_raw_msg.header.stamp = current_time.to_msg()
-                sonar_raw_msg.header.frame_id = 'sonar'
-                sonar_raw_msg.data = raw_data
-                self.get_logger().debug(str(sonar_raw_msg))
+            sonar_msg.header.stamp = current_time.to_msg()
+            sonar_msg.header.frame_id = 'sonar'
+            self.sensor.interpret_data(raw_data, sonar_msg)
 
-                sonar_msg.header.stamp = current_time.to_msg()
-                sonar_msg.header.frame_id = 'sonar'
-                self.sensor.interpret_data(raw_data, sonar_msg)
+            self.range_raw_pub.publish(sonar_raw_msg)
+            self.range_pub.publish(sonar_msg)
+            msg = String()
+            msg.data = "Active"
+            self.sonar_health.publish(msg)
 
-                self.range_raw_pub.publish(sonar_raw_msg)
-                self.range_pub.publish(sonar_msg)
-                msg = String()
-                msg.data = "Active"
-                self.sonar_health.publish(msg)
+            if self.first_exception_time:
+                self.first_exception_time = None
 
-                if self.first_exception_time:
-                    self.first_exception_time = None
+        except Exception as e:
+            msg = String()
+            msg.data = "Not Active"
+            self.sonar_health.publish(msg)
+            if self.first_exception_time is None:
+                self.get_logger().error(f'Exception when reading sonar data: {e}')
+                self.first_exception_time = current_time
+            else:
+                if current_time - self.first_exception_time > rclpy.duration.Duration(seconds=RESET_TIMEOUT):
+                    self.get_logger().error('Sonar sensor not ready')
+                    self.sensor.close_connection()
+                    self.timer.cancel()
 
-            except Exception as e:
-                msg = String()
-                msg.data = "Not Active"
-                self.sonar_health.publish(msg)
-                if self.first_exception_time is None:
-                    self.get_logger().error(f'Exception when reading sonar data: {e}')
-                    self.first_exception_time = current_time
-                else:
-                    if current_time - self.first_exception_time > rclpy.Duration(RESET_TIMEOUT):
-                        self.get_logger().error('Sonar sensor not ready')
-                        self.sensor.close_connection()
-                        break
-
-            node.sleep()
+    def destroy_node(self):
         self.sensor.close_connection()
+        super().destroy_node()
 
 def main(args=None):
     rclpy.init(args=args)
     sonar_node = SonarNode()
-    sonar_node.spin()
+    rclpy.spin(sonar_node)
+    sonar_node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
